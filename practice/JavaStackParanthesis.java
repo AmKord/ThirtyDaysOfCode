e6fdc4891548fec8e66c7aed77fddf62f306b39c
Ashok Korde Amit <amit.korde@nagra.com>
2020-11-05 18:41:18 +0530

    stack program updated

--- practice/JavaStackParanthesis.java
+++ practice/JavaStackParanthesis.java
@@ -1,7 +1,9 @@
 package com.thirtydayscode.practice;
 
+import java.security.cert.PKIXRevocationChecker.Option;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class JavaStackParanthesis {
 	
@@ -9,12 +11,44 @@ public class JavaStackParanthesis {
 		
 		List<String> stack = new ArrayList<>();
 		String item;
-		Push(String item, lst);
-		String val = Pop(lst);
+		StackOperations(stack);
+		System.out.println(stack.toString());
+		//String val = Pop(lst);
 	}
 	
 	public static void Push(String item, List<String> lst) {
 		
+		lst.add(item);
+	}
+
+	public static String Pop(List<String> lst){
+
+		String element = lst.get(lst.size()-1);
+		lst.remove(lst.size()-1);
+		return element;
+
+	}
+
+	public static void StackOperations(List<String> lst){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter records");
+		int records = sc.nextInt();
+		for (int i=0; i < records ; i++){
+			lst.add(sc.next());
+		}
+		System.out.println("Enter option, 1 push, 2 pop");
+		switch (sc.nextInt()) {
+			case 1:
+				System.out.println("enter element to insert");
+				Push(sc.next(), lst);
+				break;
+		
+			case 2:
+				System.out.println("Popped element: "+ Pop(lst));
+				break;	
+			default:
+				break;
+		}
 	}
 
 }
